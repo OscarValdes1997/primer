fc01d2027410a266f10c6b27f75166a5319b5d3e
OscarValdes1997 <313227743@pcpuma.acatlan.unam.mx>
2023-05-16 13:24:17 -0600

    Initial Commit

---
+++ primer/primerApp.swift
@@ -0,0 +1,17 @@
+//
+//  primerApp.swift
+//  primer
+//
+//  Created by Oscar Roberto Valdes Rodriguez on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct primerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
