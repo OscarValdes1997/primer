fdc77f86e001c34aa8ca95b2fb06fce80a86198a
OscarValdes1997 <313227743@pcpuma.acatlan.unam.mx>
2023-05-24 11:05:23 -0600

    todo bien

--- primer/ContentView.swift
+++ primer/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
-            DeteilList()
+        
+            RowView(RowJugador: Jugador(id: 1, nombre: "Lionel Messi", altura: "1.69", peso: "65.8", fecha: "25/agosto/97", NumUni: "19", posicion: "Delantero",equipo: "BARCELONA", IdEquipo: 1, image: Image("messi"),Escudo: Image("barca"),amarrila: "1",roja: "0",goles: "2",Asistencias: "1",idpartido: 1,partido: "barcelona vs "))
                
         }
         
